a70ce3f7ca9c8d2d8d99b0887fcd86ebbf1701e7
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-09-19 17:16:20 +0300

    dd

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-$(document).ready(function () {
-  $(".burger-menu").click(function (event) {
-    $(".header-menu").toggleClass("active");
-  });
+var button = document.getElementById("myButton");
+var buttonA = document.getElementById("myClose");
+var buttonB = document.getElementById("myHero");
+var buttonV = document.getElementById("myclos");
+
+var element = document.getElementById("formElement");
+var elamentA = document.getElementById("myElement");
+
+button.addEventListener("click", function () {
+  elementA.classList.add("is-open");
+});
+buttonA.addEventListener("click", function () {
+  elementA.classList.remove("is-open");
+});
+buttonB.addEventListener("click", function () {
+  formElement.classList.add("is-open");
+});
+buttonV.addEventListener("click", function () {
+  formElement.classList.remove("is-open");
 });
