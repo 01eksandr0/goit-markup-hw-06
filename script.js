5c0ca0525bd4b0185b99f6939c20d9aadccc0ad7
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-09-23 16:17:45 +0300

    v

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-var button = document.getElementById("myButton");
-var buttonA = document.getElementById("myClose");
-var buttonB = document.getElementById("myHero");
-var buttonV = document.getElementById("myclos");
+(() => {
+  const mobileMenu = document.querySelector(".header-menu");
+  const openMenuBtn = document.querySelector(".burger-menu");
+  const closeMenuBtn = document.querySelector(".heder-first-button-svg");
 
-var element = document.getElementById("formElement");
-var elamentA = document.getElementById("myElement");
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
 
-button.addEventListener("click", function () {
-  elementA.classList.add("is-open");
-});
-buttonA.addEventListener("click", function () {
-  elementA.classList.remove("is-open");
-});
-buttonB.addEventListener("click", function () {
-  formElement.classList.add("is-open");
-});
-buttonV.addEventListener("click", function () {
-  formElement.classList.remove("is-open");
-});
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
+// ================================================================
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
